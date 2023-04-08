f3fb15dc14ecc6c8ce6cb627b27c47e5336cfa24
TomekPLR <trudzki@gmail.com>
2023-04-08 22:54:35 +0200

    Update property_explorer.py
    Now there are two additional controls for selecting variables to compare over time, and the correlation matrix displays the correlation values.

--- property_explorer.py
+++ property_explorer.py
@@ -18,13 +18,28 @@ def display_correlation_matrix(df):
     f, ax = plt.subplots(figsize=(11, 9))
     cmap = sns.diverging_palette(230, 20, as_cmap=True)
     sns.heatmap(corr, mask=mask, cmap=cmap, vmax=1, vmin=-1, center=0,
-                square=True, linewidths=.5, cbar_kws={"shrink": .5})
+                square=True, linewidths=.5, cbar_kws={"shrink": .5}, annot=True, fmt=".2f")
     st.pyplot(f)
 
 def plot_column(df, column):
     fig, ax = plt.subplots(figsize=(12, 6))
     sns.lineplot(data=df, x='scrap_date', y=column, ax=ax)
     plt.title(f'Trend of {column} over time')
+    st.pyplot(fig)
+
+def plot_compare_columns(df, col1, col2):
+    fig, ax1 = plt.subplots(figsize=(12, 6))
+    ax1.plot(df['scrap_date'], df[col1], label=col1, color='blue')
+    ax1.set_ylabel(col1, color='blue')
+    ax1.tick_params(axis='y', labelcolor='blue')
+
+    ax2 = ax1.twinx()
+    ax2.plot(df['scrap_date'], df[col2], label=col2, color='red')
+    ax2.set_ylabel(col2, color='red')
+    ax2.tick_params(axis='y', labelcolor='red')
+
+    fig.tight_layout()
+    plt.title(f'Comparison of {col1} and {col2} over time')
     st.pyplot(fig)
 
 def main():
@@ -46,11 +61,17 @@ def main():
         numerical_columns = [col for col in property_df.columns if np.issubdtype(property_df[col].dtype, np.number) and col != 'scrap_date']
         chart_selection = st.sidebar.selectbox('Select a chart', [''] + numerical_columns)
 
+        st.sidebar.header('Compare two variables')
+        col1 = st.sidebar.selectbox('Select first variable', [''] + numerical_columns)
+        col2 = st.sidebar.selectbox('Select second variable', [''] + numerical_columns)
+
         if chart_selection:
             plot_column(property_df, chart_selection)
         else:
             for col in numerical_columns:
                 plot_column(property_df, col)
 
-if __name__ == '__main__':
-    main()
+        if col1 and col2 and col1 != col2:
+            st.header(f'Comparison of {col1} and {col2}')
+            plot_compare_columns(property_df, col1, col2)
+
