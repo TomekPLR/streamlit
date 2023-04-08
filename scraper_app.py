97b8968d59bc31844f84d61bcead4f6a50af14ef
TomekPLR <trudzki@gmail.com>
2023-04-08 20:44:30 +0200

    Update scraper_app.py

--- scraper_app.py
+++ scraper_app.py
@@ -1,61 +1,64 @@
-import pandas as pd
 import streamlit as st
-import numpy as np
-import datetime
+import pandas as pd
+from pandas.api.types import CategoricalDtype
+
+# Define a custom sorting order for the "property" column
+property_order = ['Property A', 'Property B', 'Property C', 'Property D', 'Property E']
 
 # Load the CSV file
-df = pd.read_csv("scraper_data_all_properties.csv")
-df["scrap_date"] = pd.to_datetime(df["scrap_date"])
+data = pd.read_csv("data.csv")
 
-# Get list of all properties
-all_properties = df["property"].unique()
+# Convert the "scrap_date" column to a categorical data type and sort it in ascending order
+date_order = sorted(data["scrap_date"].unique())
+date_dtype = CategoricalDtype(categories=date_order, ordered=True)
+data["scrap_date"] = data["scrap_date"].astype(date_dtype)
 
-# Define function to calculate change percentage
-def change_pct(initial, final):
-    if initial == 0:
-        return np.nan
+# Define the columns that can be selected for analysis
+analysis_columns = ['download_size', 'requests', 'avg_response_time', 'response_ok', 'response_301', 'response_302', 'response_404', 'response_server_error', 'clicks', 'impressions', 'avg_ctr', 'avg_position', 'purpose_discovery', 'purpose_refresh', 'mobile_good', 'mobile_improve', 'mobile_poor', 'desktop_good', 'desktop_improve', 'desktop_poor']
+
+# Define the column names to use for the output table
+output_columns = ['Property', 'Initial Value', 'Final Value', 'Absolute Change', 'Relative Change']
+
+# Define a function to calculate the relative change
+def calculate_relative_change(initial_value, final_value):
+    if initial_value == 0:
+        return 0
     else:
-        return (final - initial) / initial * 100
+        return (final_value - initial_value) / initial_value
 
-# Define function to filter data by date and property, and sort by column
-def filter_and_sort(df, start_date, end_date, property_name, sort_col):
-    # Filter by date and property
-    filtered_df = df[(df["scrap_date"] >= start_date) & (df["scrap_date"] <= end_date)]
-    if property_name != "All":
-        filtered_df = filtered_df[filtered_df["property"] == property_name]
+# Define the Streamlit app
+st.title("Property Analysis")
 
-    # Calculate initial and final values
-    initial_values = filtered_df.groupby("property")[sort_col].first().rename("initial")
-    final_values = filtered_df.groupby("property")[sort_col].last().rename("final")
-    change_values = final_values - initial_values
-    pct_change_values = change_pct(initial_values, final_values)
+# Sidebar controls
+initial_date = st.sidebar.selectbox("Select initial date", date_order)
+final_date = st.sidebar.selectbox("Select final date", date_order)
+analysis_column = st.sidebar.selectbox("Select column to analyze", analysis_columns)
 
-    # Combine into a new DataFrame and sort by pct_change
-    result_df = pd.concat([initial_values, final_values, change_values, pct_change_values], axis=1)
-    result_df = result_df.sort_values("pct_change", ascending=False)
+# Filter the data based on the selected dates
+filtered_data = data[(data["scrap_date"] == initial_date) | (data["scrap_date"] == final_date)]
 
-    return result_df
+# Calculate the initial and final values for each property
+property_data = filtered_data.pivot_table(values=analysis_column, index="property", columns="scrap_date")
+property_data["Absolute Change"] = property_data[final_date] - property_data[initial_date]
+property_data["Relative Change"] = property_data.apply(lambda row: calculate_relative_change(row[initial_date], row[final_date]), axis=1)
 
+# Sort the properties by their relative change
+sorted_data = property_data.sort_values("Relative Change", ascending=False)
 
-# Create Streamlit app
-st.title("Scraper Data Analysis")
-st.sidebar.title("Filters")
+# Display the top 10 winning properties
+st.subheader("Top 10 winning properties")
+winning_properties = sorted_data.head(10)[output_columns]
+winning_properties.index.name = None
+st.dataframe(winning_properties.style.format({'Relative Change': "{:.2%}"}))
 
-# Date range filter
-min_date = df["scrap_date"].min().date()
-max_date = df["scrap_date"].max().date()
-start_date = st.sidebar.date_input("Start date", min_date)
-end_date = st.sidebar.date_input("End date", max_date)
+# Display the top 10 losing properties
+st.subheader("Top 10 losing properties")
+losing_properties = sorted_data.tail(10)[output_columns]
+losing_properties.index.name = None
+st.dataframe(losing_properties.style.format({'Relative Change': "{:.2%}"}))
 
-# Property filter
-property_name = st.sidebar.selectbox("Property", ["All"] + list(all_properties))
-
-# Column to sort by filter
-sort_col = st.sidebar.selectbox("Sort by", df.columns)
-
-# Filter and sort data
-result_df = filter_and_sort(df, start_date, end_date, property_name, sort_col)
-
-# Show the results in a table
-st.write(result_df.head(10))
-st.write(result_df.tail(10))
+# Display the full table
+st.subheader("Full table")
+full_table = property_data[output_columns].reindex(property_order)
+full_table.index.name = None
+st.dataframe(full_table.style.format({'Relative Change': "{:.2%}"}))
