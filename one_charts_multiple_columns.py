cc0cfd478f84ac654454d9438c0b43e45a245ffd
TomekPLR <trudzki@gmail.com>
2023-04-09 14:15:44 +0200

    Update one_charts_multiple_columns.py
    add a normalization function

--- one_charts_multiple_columns.py
+++ one_charts_multiple_columns.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+from sklearn.preprocessing import MinMaxScaler
+
+def normalize_columns(df, columns):
+    scaler = MinMaxScaler()
+    normalized_df = df.copy()
+    normalized_df[columns] = scaler.fit_transform(df[columns])
+    return normalized_df
 
 def main():
-    st.title("Single Chart Multi-column Visualization")
+    st.title("Normalized Single Chart Multi-column Visualization")
 
     uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
     if uploaded_file is not None:
@@ -23,7 +30,8 @@ def main():
         if len(column_options) > 0:
             st.subheader("Chart")
             filtered_df = df[df['property'] == selected_property]
-            chart_data = filtered_df[['scrap_date'] + column_options].melt('scrap_date', var_name='variable', value_name='value')
+            normalized_df = normalize_columns(filtered_df, column_options)
+            chart_data = normalized_df[['scrap_date'] + column_options].melt('scrap_date', var_name='variable', value_name='value')
             chart = alt.Chart(chart_data).mark_line().encode(
                 x='scrap_date:T',
                 y='value:Q',
