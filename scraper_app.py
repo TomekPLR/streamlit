55783e86ac12358e5cfe0f4961a725ad1cb9d403
TomekPLR <trudzki@gmail.com>
2023-04-11 19:19:48 +0200

    Update scraper_app.py
    add property explorer - charts

--- scraper_app.py
+++ scraper_app.py
@@ -1,23 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from datetime import datetime
 
-def read_csv(uploaded_files):
-    dfs = []
-
-    for uploaded_file in uploaded_files:
-        df = pd.read_csv(uploaded_file)
-        df['scrap_date'] = pd.to_datetime(df['scrap_date']).dt.date
-        dfs.append(df)
-
-    combined_df = pd.concat(dfs, axis=0, ignore_index=True)
-    return combined_df
+def read_csv(uploaded_file):
+    df = pd.read_csv(uploaded_file)
+    df['scrap_date'] = pd.to_datetime(df['scrap_date']).dt.date
+    return df
 
 def filter_data(df, start_date, end_date):
     return df[(df['scrap_date'] >= start_date) & (df['scrap_date'] <= end_date)]
 
-def calculate_changes(df, column, clicks_column='clicks'):
+def calculate_changes(df, column):
     if not np.issubdtype(df[column].dtype, np.number):
         raise ValueError("Selected column must be of numeric data type")
 
@@ -28,20 +23,8 @@ def calculate_changes(df, column, clicks_column='clicks'):
 
     results = pd.concat([initial_values, final_values, abs_changes, rel_changes], axis=1)
     results.columns = ['initial_value', 'final_value', 'abs_change', 'rel_change']
-
-    if clicks_column in df.columns:
-        initial_clicks = df.groupby('property')[clicks_column].first()
-        final_clicks = df.groupby('property')[clicks_column].last()
-        click_changes = final_clicks - initial_clicks
-        rel_click_changes = (final_clicks - initial_clicks) / initial_clicks * 100
-
-        results = pd.concat([results, click_changes, rel_click_changes], axis=1)
-        results.columns = ['initial_value', 'final_value', 'abs_change', 'rel_change', 'clicks_change', 'rel_clicks_change']
-
     results = results.reset_index()
     return results
-
-
 
 def filter_properties(df, property_filter, selected_property):
     if property_filter:
@@ -53,9 +36,10 @@ def filter_properties(df, property_filter, selected_property):
 def main():
     st.title('CSV Analysis Tool')
 
-    uploaded_files = st.file_uploader("Upload CSV files", type="csv", accept_multiple_files=True)
-    if uploaded_files:
-        df = read_csv(uploaded_files)
+    uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
+    if uploaded_file is not None:
+        df = read_csv(uploaded_file)
+
         st.sidebar.header('Filter data')
         min_date, max_date = df['scrap_date'].min(), df['scrap_date'].max()
         start_date = st.sidebar.date_input('Initial date', min_date, min_value=min_date, max_value=max_date)
@@ -90,6 +74,38 @@ def main():
         st.header(f'Top {num_properties} properties with decreased {column} values')
         top_losers = results.nsmallest(num_properties, 'abs_change')
         st.dataframe(top_losers)
+
+        st.header('Property-specific chart')
+        property_to_plot = st.selectbox('Select a property to plot', [''] + list(df['property'].unique()))
+        normalize = st.checkbox('Normalize data')
+         start_date_chart = st.date_input('Initial date for chart', min_date, min_value=min_date, max_value=max_date)
+        end_date_chart = st.date_input('Final date for chart', max_date, min_value=min_date, max_value=max_date)
+
+        if property_to_plot:
+            property_df = filter_data(df[df['property'] == property_to_plot], start_date_chart, end_date_chart)
+
+            if 'clicks' in property_df.columns:
+                fig, ax = plt.subplots()
+
+                # Plot clicks data
+                ax.plot(property_df['scrap_date'], property_df['clicks'], label='Clicks')
+
+                # Choose another column to plot
+                other_column = 'avg_response_time'  # Change this to the column you want to plot
+                if other_column in property_df.columns:
+                    ax.plot(property_df['scrap_date'], property_df[other_column], label=other_column)
+
+                if normalize:
+                    ax.set_yscale('log')
+
+                ax.legend()
+                ax.set_xlabel('Date')
+                ax.set_ylabel('Value')
+                ax.set_title(f'Property: {property_to_plot}')
+
+                st.pyplot(fig)
+            else:
+                st.write(f"No 'clicks' data available for property {property_to_plot}.")
 
 if __name__ == '__main__':
     main()
