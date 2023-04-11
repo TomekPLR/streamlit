43591661b162c5abfd636c406e33c6caafa1f9ee
TomekPLR <trudzki@gmail.com>
2023-04-11 20:42:02 +0200

    Update scraper_app.py

--- scraper_app.py
+++ scraper_app.py
@@ -20,6 +20,20 @@ def filter_data(df, start_date, end_date):
 def get_unique_dates(df):
     return df['scrap_date'].min(), df['scrap_date'].max()
 
+def calculate_changes(df, column):
+    if not np.issubdtype(df[column].dtype, np.number):
+        raise ValueError("Selected column must be of numeric data type")
+
+    initial_values = df.groupby('property')[column].first()
+    final_values = df.groupby('property')[column].last()
+    abs_changes = final_values - initial_values
+    rel_changes = (final_values - initial_values) / initial_values * 100
+
+    results = pd.concat([initial_values, final_values, abs_changes, rel_changes], axis=1)
+    results.columns = ['initial_value', 'final_value', 'abs_change', 'rel_change']
+    results = results.reset_index()
+    return results
+
 def main():
     st.title('CSV Analysis Tool')
 
@@ -37,6 +51,26 @@ def main():
 
         filtered_df = filter_data(df, start_date, end_date)
 
+        st.sidebar.header('Select a column')
+        column = st.sidebar.selectbox('Choose a column', df.columns)
+
+        try:
+            results = calculate_changes(filtered_df, column)
+        except ValueError as e:
+            st.sidebar.error(str(e))
+            return
+
+        st.sidebar.header('Report settings')
+        num_properties = st.sidebar.number_input('Number of reported properties', min_value=1, value=10, step=1)
+
+        st.header(f'Top {num_properties} properties with increased {column} values')
+        top_winners = results.nlargest(num_properties, 'abs_change')
+        st.dataframe(top_winners)
+
+        st.header(f'Top {num_properties} properties with decreased {column} values')
+        top_losers = results.nsmallest(num_properties, 'abs_change')
+        st.dataframe(top_losers)
+
         specific_property = st.selectbox("Select a property", [""] + list(df["property"].unique()))
         if specific_property:
             property_data = df[df["property"] == specific_property]
@@ -46,31 +80,29 @@ def main():
             chart_columns = st.multiselect("Select columns to plot", options=df.columns, default=["clicks"])
 
             min_chart_date, max_chart_date = get_unique_dates(property_data)
-            chart_start_date = st.date_input("Chart start date", min_chart_date, min_value=min_chart_date, max_value=max_chart_date)
-            chart_end_date = st.date_input("Chart end date", max_chart_date, min_value=min_chart_date, max_value=max_chart_date)
-
+                        chart_start_date = st.date_input('Chart start date', min_chart_date, min_value=min_chart_date, max_value=max_chart_date)
+            chart_end_date = st.date_input('Chart end date', max_chart_date, min_value=min_chart_date, max_value=max_chart_date)
             if chart_start_date > chart_end_date:
-                st.error("Error: Chart end date must be after chart start date.")
+                st.error('Error: Chart end date must be after chart start date.')
             else:
-                property_chart_data = filter_data(property_data, chart_start_date, chart_end_date)
+                chart_data = filter_data(property_data, chart_start_date, chart_end_date)
 
-                normalize = st.checkbox("Normalize values")
+                if st.checkbox("Normalize data"):
+                    for col in chart_columns:
+                        if np.issubdtype(chart_data[col].dtype, np.number):
+                            chart_data[col] = (chart_data[col] - chart_data[col].min()) / (chart_data[col].max() - chart_data[col].min())
 
-                if normalize:
-                    property_chart_data[chart_columns] = (property_chart_data[chart_columns] - property_chart_data[chart_columns].min()) / (property_chart_data[chart_columns].max() - property_chart_data[chart_columns].min())
+                chart_data = chart_data.melt(id_vars=['scrap_date'], value_vars=chart_columns, var_name='column', value_name='value')
 
-                chart = alt.Chart(property_chart_data).mark_line().encode(
-                    x="scrap_date:T",
-                    y=alt.Y(alt.repeat("row"), type="quantitative"),
-                    color="property:N"
-                ).properties(
-                    width=600,
-                    height=200
-                ).repeat(
-                    row=chart_columns
+                line_chart = alt.Chart(chart_data).mark_line().encode(
+                    alt.X('scrap_date:T', title='Scrap Date'),
+                    alt.Y('value:Q', title='Value'),
+                    alt.Color('column:N', title='Column'),
+                    tooltip=['scrap_date', 'column', 'value']
                 ).interactive()
 
-                st.altair_chart(chart)
+                st.altair_chart(line_chart, use_container_width=True)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
