6d98fcd9c34fa6a43aa7115e71969c38b1eeea3a
TomekPLR <trudzki@gmail.com>
2023-04-14 13:08:25 +0200

    Update correlation_charts.py

--- correlation_charts.py
+++ correlation_charts.py
@@ -2,59 +2,70 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Load the CSV data
-@st.cache
-def load_data(file):
-    data = pd.read_csv(file, parse_dates=['scrap_date'])
-    return data
+# Sidebar selectors
+st.sidebar.header("Settings")
+uploaded_file = st.sidebar.file_uploader("Upload your CSV file", type=["csv"])
 
-st.title('CSV Data Analysis')
-
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
-    data = load_data(uploaded_file)
+    # Load the CSV
+    data = pd.read_csv(uploaded_file)
 
-    # Select the initial and final dates
-    now = datetime.datetime.now()
-    initial_date = pd.Timestamp(st.date_input('Initial date', now - datetime.timedelta(weeks=2)))
-    final_date = pd.Timestamp(st.date_input('Final date', now))
+    # Convert scrap_date column to pandas datetime format
+    data['scrap_date'] = pd.to_datetime(data['scrap_date'])
 
+    now = datetime.date.today()
+    initial_date = pd.Timestamp(st.sidebar.date_input('Initial date', now - datetime.timedelta(weeks=2)))
+    final_date = pd.Timestamp(st.sidebar.date_input('Final date', now))
+    property_filter = st.sidebar.multiselect("Select properties", data['property'].unique())
+
+    # Filter the data based on the selected dates
     filtered_data = data[(data['scrap_date'] >= initial_date) & (data['scrap_date'] <= final_date)]
 
-    # Select variables and filter properties
-    variable1 = st.selectbox('Select variable 1', filtered_data.columns)
-    variable2 = st.selectbox('Select variable 2', filtered_data.columns)
-    properties = st.multiselect('Select properties', filtered_data['property'].unique())
-    num_results = st.number_input('Number of results', min_value=1, value=100, step=1)
+    # Filter the data based on the selected properties
+    if property_filter:
+        filtered_data = filtered_data[filtered_data['property'].isin(property_filter)]
 
-    if properties:
-        filtered_data = filtered_data[filtered_data['property'].isin(properties)]
+    # Select columns
+    variable1 = st.sidebar.selectbox("Select variable 1", filtered_data.columns)
+    variable2 = st.sidebar.selectbox("Select variable 2", filtered_data.columns)
 
-    def calculate_correlation(data, var1, var2):
-        if np.issubdtype(data[var1].dtype, np.number) and np.issubdtype(data[var2].dtype, np.number):
-            return data[[var1, var2]].corr().iloc[0, 1]
+    # Select sorting order
+    sort_order = st.sidebar.selectbox("Sort by correlation", ["Highest", "Lowest"])
+
+    # Normalize option
+    normalize = st.sidebar.checkbox("Normalize charts (0-1)")
+
+    # Calculate correlations
+    correlations = filtered_data.groupby("property").apply(lambda x: x[variable1].corr(x[variable2]))
+
+    # Sort properties based on the correlation
+    if sort_order == "Highest":
+        sorted_properties = correlations.sort_values(ascending=False)
+    else:
+        sorted_properties = correlations.sort_values(ascending=True)
+
+    # Display charts
+    st.title("Charts")
+
+    for property in sorted_properties.index[:100]:
+        st.header(f"Property: {property}")
+        st.write(f"Correlation: {sorted_properties[property]}")
+        st.write(f"Difference in number of clicks: {filtered_data.loc[filtered_data['property'] == property, 'clicks'].diff().sum()}")
+
+        property_data = filtered_data[filtered_data['property'] == property]
+        fig, ax = plt.subplots()
+
+        if normalize:
+            ax.plot((property_data[variable1] - property_data[variable1].min()) / (property_data[variable1].max() - property_data[variable1].min()), label=variable1)
+            ax.plot((property_data[variable2] - property_data[variable2].min()) / (property_data[variable2].max() - property_data[variable2].min()), label=variable2)
         else:
-            return None
+            ax.plot(property_data[variable1], label=variable1)
+            ax.plot(property_data[variable2], label=variable2)
 
-    # Calculate correlations and sort properties
-    correlations = filtered_data.groupby('property').apply(lambda x: calculate_correlation(x, variable1, variable2))
-    correlations = correlations.nlargest(num_results)
-
-    # Display the results
-    for property_name, correlation in correlations.items():
-        st.write(f'Property: {property_name}')
-        st.write(f'Correlation between {variable1} and {variable2}: {correlation}')
-        
-        if correlation is not None:
-            property_data = filtered_data[filtered_data['property'] == property_name]
-            st.line_chart(property_data[[variable1, variable2]])
-
-        before = property_data[property_data['scrap_date'] == initial_date]
-        after = property_data[property_data['scrap_date'] == final_date]
-
-        if not before.empty and not after.empty:
-            st.write(f"Difference in clicks: {after['clicks'].values[0] - before['clicks'].values[0]}")
-            st.write(f"Difference in {variable1}: {after[variable1].values[0] - before[variable1].values[0]}")
-            st.write(f"Difference in {variable2}: {after[variable2].values[0] - before[variable2].values[0]}")
-        st.write('\n\n')
+        ax.legend()
+        st.pyplot(fig)
+else:
+    st.sidebar.warning("Please upload a CSV file.")
