d06a27698c9cd5eb904600305b178cb76eed62d9
TomekPLR <trudzki@gmail.com>
2023-04-14 12:57:34 +0200

    Update correlation_charts.py

--- correlation_charts.py
+++ correlation_charts.py
@@ -1,59 +1,61 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
+import datetime
 
-# File uploader
+# Load the CSV data
+@st.cache
+def load_data(file):
+    data = pd.read_csv(file, parse_dates=['scrap_date'])
+    return data
+
+st.title('CSV Data Analysis')
+
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-
 if uploaded_file is not None:
-    # Read the CSV file
-    df = pd.read_csv(uploaded_file)
+    data = load_data(uploaded_file)
 
-    # Set default dates
-    today = pd.Timestamp.today()
-    default_start_date = (today - pd.Timedelta(weeks=2)).strftime("%Y-%m-%d")
-    default_end_date = today.strftime("%Y-%m-%d")
+    # Select the initial and final dates
+    now = datetime.datetime.now()
+    initial_date = st.date_input('Initial date', now - datetime.timedelta(weeks=2))
+    final_date = st.date_input('Final date', now)
 
-    # Sidebar inputs
-    start_date = st.sidebar.date_input("Start date", pd.to_datetime(default_start_date))
-    end_date = st.sidebar.date_input("End date", pd.to_datetime(default_end_date))
-    variable1 = st.sidebar.selectbox("Variable 1", df.columns)
-    variable2 = st.sidebar.selectbox("Variable 2", df.columns)
-    property = st.sidebar.selectbox("Property", df["property"].unique(), index=0)
-    num_results = st.sidebar.number_input("Number of results", min_value=1, max_value=len(df), value=100)
+    # Filter the data based on the selected dates
+    filtered_data = data[(data['scrap_date'] >= initial_date) & (data['scrap_date'] <= final_date)]
 
-    # Filter the data by date and property
-    mask = (df["scrap_date"] >= start_date) & (df["scrap_date"] <= end_date)
-    if property != "All":
-        mask &= df["property"] == property
-    filtered_df = df.loc[mask]
+    # Select variables and filter properties
+    variable1 = st.selectbox('Select variable 1', filtered_data.columns)
+    variable2 = st.selectbox('Select variable 2', filtered_data.columns)
+    properties = st.multiselect('Select properties', filtered_data['property'].unique())
+    num_results = st.number_input('Number of results', min_value=1, value=100, step=1)
 
-    # Calculate the correlation between variable1 and variable2 for each property
-    correlations = filtered_df.groupby("property")[variable1, variable2].corr().iloc[::2, -1]
-    sorted_properties = correlations.sort_values(ascending=False).index[:num_results]
+    if properties:
+        filtered_data = filtered_data[filtered_data['property'].isin(properties)]
 
-    # Display the plots and text information for each property
-    for prop in sorted_properties:
-        prop_df = filtered_df[filtered_df["property"] == prop]
-        x = prop_df[variable1]
-        y = prop_df[variable2]
-        corr = correlations[prop]
-        diff_clicks = prop_df["clicks_after"].sum() - prop_df["clicks_before"].sum()
-        diff_var1 = prop_df[variable1].max() - prop_df[variable1].min()
-        diff_var2 = prop_df[variable2].max() - prop_df[variable2].min()
+    def calculate_correlation(data, var1, var2):
+        if np.issubdtype(data[var1].dtype, np.number) and np.issubdtype(data[var2].dtype, np.number):
+            return data[[var1, var2]].corr().iloc[0, 1]
+        else:
+            return None
 
-        # Normalize x and y to be between 0 and 1
-        x_norm = (x - x.min()) / (x.max() - x.min())
-        y_norm = (y - y.min()) / (y.max() - y.min())
+    # Calculate correlations and sort properties
+    correlations = filtered_data.groupby('property').apply(lambda x: calculate_correlation(x, variable1, variable2))
+    correlations = correlations.nlargest(num_results)
 
-        # Plot the normalized data
-        fig, ax = plt.subplots()
-        sns.scatterplot(x=x_norm, y=y_norm, ax=ax)
-        ax.set_title(f"{prop} (Corr: {corr:.2f})")
-        st.pyplot(fig)
+    # Display the results
+    for property_name, correlation in correlations.items():
+        st.write(f'Property: {property_name}')
+        st.write(f'Correlation between {variable1} and {variable2}: {correlation}')
+        
+        if correlation is not None:
+            property_data = filtered_data[filtered_data['property'] == property_name]
+            st.line_chart(property_data[[variable1, variable2]])
 
-        # Display the text information
-        st.write(f"Difference in clicks: {diff_clicks}")
-        st.write(f"Difference in {variable1}: {diff_var1}")
-        st.write(f"Difference in {variable2}: {diff_var2}")
+        before = property_data[property_data['scrap_date'] == initial_date]
+        after = property_data[property_data['scrap_date'] == final_date]
+
+        if not before.empty and not after.empty:
+            st.write(f"Difference in clicks: {after['clicks'].values[0] - before['clicks'].values[0]}")
+            st.write(f"Difference in {variable1}: {after[variable1].values[0] - before[variable1].values[0]}")
+            st.write(f"Difference in {variable2}: {after[variable2].values[0] - before[variable2].values[0]}")
+        st.write('\n\n')
