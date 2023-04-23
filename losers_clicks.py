d33c4e05b55d6cac354740df86a514780ea4508d
TomekPLR <trudzki@gmail.com>
2023-04-23 15:22:09 +0200

    Update losers_clicks.py

--- losers_clicks.py
+++ losers_clicks.py
@@ -5,22 +5,24 @@ import matplotlib.pyplot as plt
 
 st.title('CSV Analysis - Property Click Losses')
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file, parse_dates=['scrap_date'])
     data['date'] = data['scrap_date'].dt.date
 
-    initial_date = st.date_input('Initial Date', data['date'].min())
-    final_date = st.date_input('Final Date', data['date'].max())
+    initial_date = st.sidebar.date_input('Initial Date', data['date'].min())
+    final_date = st.sidebar.date_input('Final Date', data['date'].max())
 
     if initial_date > final_date:
         st.error("Initial date should be before final date.")
     else:
         filtered_data = data[(data['date'] >= initial_date) & (data['date'] <= final_date)]
 
-        property_filter = st.text_input('Filter properties by name (contains):', '')
+        property_filter = st.sidebar.text_input('Filter properties by name (contains):', '')
         if property_filter:
             filtered_data = filtered_data[filtered_data['property'].str.contains(property_filter)]
+
+        min_clicks = st.sidebar.slider("Minimum clicks threshold:", 0, int(filtered_data['clicks'].max()), 0)
 
         # Compute the click losses based on the initial and final dates
         grouped_data = filtered_data.pivot_table(index='property', columns='date', values='clicks', aggfunc='first').reset_index()
@@ -28,16 +30,17 @@ if uploaded_file is not None:
         grouped_data['absolute_loss'] = grouped_data[initial_date] - grouped_data[final_date]
         grouped_data['relative_loss'] = (grouped_data['absolute_loss'] / grouped_data[initial_date]) * 100
 
-        # Clean up the DataFrame
+        # Clean up the DataFrame and apply the minimum clicks threshold
         grouped_data = grouped_data[['property', initial_date, final_date, 'absolute_loss', 'relative_loss']]
         grouped_data.columns.name = None
         grouped_data.rename(columns={initial_date: 'initial_clicks', final_date: 'final_clicks'}, inplace=True)
+        grouped_data = grouped_data[grouped_data['initial_clicks'] >= min_clicks]
 
-        num_properties = st.slider("Number of properties to display in the losers table:", 1, len(grouped_data), 10)
-        st.write(f'Biggest {num_properties} losers in terms of clicks between selected dates:')
+        num_properties = st.sidebar.slider("Number of properties to display in the losers table:", 1, len(grouped_data), 10)
+        st.write(f'Biggest {num_properties} losers in terms of clicks between selected dates (minimum {min_clicks} clicks):')
         st.write(grouped_data.sort_values(by='absolute_loss', ascending=False).head(num_properties))
 
-        property_chart = st.selectbox('Select a property to see the chart:', grouped_data['property'].unique())
+        property_chart = st.sidebar.selectbox('Select a property to see the chart:', grouped_data['property'].unique())
         selected_property = filtered_data[filtered_data['property'] == property_chart]
 
         fig, ax = plt.subplots()
