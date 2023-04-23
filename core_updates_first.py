ad2fce9bf669c9e3ca3d6626ad9440229c012bc3
TomekPLR <trudzki@gmail.com>
2023-04-23 19:05:08 +0200

    Update core_updates_first.py
    major program changes

--- core_updates_first.py
+++ core_updates_first.py
@@ -8,16 +8,7 @@ import datetime
 st.title('CSV Analysis - Property Click Losses')
 
 important_dates = {
-    '2022-12-14': 'Dec 2022 link spam update',
-    '2022-12-05': 'Dec 2022 helpful content update',
-    '2022-10-19': 'Oct 2022 spam update',
-    '2022-09-20': 'Sep 2022 product reviews update',
-    '2022-09-12': 'Sep 2022 core update',
-    '2022-08-25': 'Aug 2022 helpful content update',
-    '2022-07-27': 'Jul 2022 product reviews update',
-    '2022-05-25': 'May 2022 core update',
-    '2022-03-23': 'Mar 2022 product reviews update',
-    '2022-02-22': 'Feb 2022 page experience update for desktop',
+    # (same content as before)
 }
 
 # Convert the keys to datetime objects
@@ -30,6 +21,14 @@ if uploaded_file is not None:
 
     initial_date = st.sidebar.date_input('Initial Date', data['date'].min())
     final_date = st.sidebar.date_input('Final Date', data['date'].max())
+
+    additional_dates = st.sidebar.text_area("Add additional important dates (YYYY-MM-DD:description, one per line):")
+
+    if additional_dates:
+        additional_dates_list = additional_dates.split('\n')
+        for date_entry in additional_dates_list:
+            date, description = date_entry.split(':')
+            important_dates[pd.to_datetime(date)] = description
 
     if initial_date > final_date:
         st.error("Initial date should be before final date.")
@@ -49,7 +48,20 @@ if uploaded_file is not None:
         filtered_properties = filtered_data['property'].unique()
         st.write(f"Found {len(filtered_properties)} properties")
 
+        drop_summary = []
+
         for property_name in filtered_properties:
+            selected_property = filtered_data[filtered_data['property'] == property_name]
+            start_clicks = selected_property[selected_property['date'] == initial_date]['clicks'].mean()
+            end_clicks = selected_property[selected_property['date'] == final_date]['clicks'].mean()
+
+            if start_clicks is not None and end_clicks is not None:
+                drop_percentage = ((start_clicks - end_clicks) / start_clicks) * 100
+                drop_summary.append((property_name, drop_percentage))
+
+        sorted_properties = sorted(drop_summary, key=lambda x: x[1], reverse=True)
+
+        for property_name, drop_percentage in sorted_properties:
             selected_property = filtered_data[filtered_data['property'] == property_name]
 
             if selected_property['clicks'].mean() >= threshold:
@@ -58,7 +70,7 @@ if uploaded_file is not None:
                 ax.plot(selected_property['scrap_date'], selected_property['clicks'], label=property_name)
                 ax.set_xlabel('Scrap Date')
                 ax.set_ylabel('Clicks')
-                ax.set_title(f'{property_name} Clicks over Time', pad=20)
+                ax.set_title(f'{property_name} Clicks over Time ({drop_percentage:.2f}% drop)', pad=20)
 
                 for event_date, event_description in important_dates.items():
                     if event_description in selected_dates:
