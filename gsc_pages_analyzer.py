daa76862efb2df560d7900b203c6a52733acd4ec
TomekPLR <trudzki@gmail.com>
2023-04-23 22:27:31 +0200

    Update gsc_pages_analyzer.py

--- gsc_pages_analyzer.py
+++ gsc_pages_analyzer.py
@@ -8,6 +8,10 @@ from datetime import datetime, timedelta
 default_end_date = datetime.today() - timedelta(days=1)
 default_start_date = default_end_date - timedelta(days=14)
 
+# Define the default number of countries and catalogs to show
+default_num_countries = 5
+default_num_catalogs = 5
+
 # Define the file uploader
 uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
 
@@ -21,24 +25,26 @@ if uploaded_file is not None:
     # Define the date range selector
     start_date = st.sidebar.date_input("Select a start date", default_start_date)
     end_date = st.sidebar.date_input("Select an end date", default_end_date)
-
-    # Convert start and end date to datetime format
     start_date = datetime.strptime(str(start_date), '%Y-%m-%d')
     end_date = datetime.strptime(str(end_date), '%Y-%m-%d')
 
     # Filter the pages data by date range
     pages_filtered = pages_df[(pages_df["Date"] >= start_date) & (pages_df["Date"] <= end_date)]
 
-    # Create a line chart of clicks by country and date
+    # Create a chart of clicks by country and date
     clicks_by_country = pages_filtered.groupby(["Country", "Date"])["Url Clicks"].sum().reset_index()
+    top_countries = clicks_by_country.groupby("Country")["Url Clicks"].sum().sort_values(ascending=False).head(default_num_countries)
+    countries_selected = st.sidebar.multiselect("Select countries:", top_countries.index, default=top_countries.index[:default_num_countries])
+    clicks_by_country_filtered = clicks_by_country[clicks_by_country["Country"].isin(countries_selected)]
     fig1, ax1 = plt.subplots()
-    sns.lineplot(x="Date", y="Url Clicks", hue="Country", data=clicks_by_country, ax=ax1)
+    sns.lineplot(x="Date", y="Url Clicks", hue="Country", data=clicks_by_country_filtered, ax=ax1)
     ax1.set_title("Clicks by Country")
     st.pyplot(fig1)
 
     # Define the catalog selector
-    catalogs = pages_filtered["Landing Page"].apply(lambda x: x.split("/")[1]).value_counts().nlargest(5)
-    catalogs_selected = st.sidebar.multiselect("Select catalogs:", catalogs.index, default=catalogs.index)
+    catalogs = pages_filtered["Landing Page"].apply(lambda x: x.split("/")[1]).unique()
+    top_catalogs = pages_filtered.groupby("Landing Page")["Url Clicks"].sum().sort_values(ascending=False).head(default_num_catalogs)
+    catalogs_selected = st.sidebar.multiselect("Select catalogs:", top_catalogs.index, default=top_catalogs.index[:default_num_catalogs])
 
     # Define the metric selector
     metric = st.sidebar.selectbox("Select a metric:", ["Url Clicks", "Impressions", "URL CTR"])
