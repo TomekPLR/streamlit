87c5640e80638b20f59ef174922ae522d08ba73d
TomekPLR <trudzki@gmail.com>
2023-04-23 17:05:32 +0200

    Update core_updates_first.py

--- core_updates_first.py
+++ core_updates_first.py
@@ -8,16 +8,7 @@ import datetime
 st.title('CSV Analysis - Property Click Losses')
 
 important_dates = {
-    '2022-12-14': 'Dec 2022 link spam update',
-    '2022-12-05': 'Dec 2022 helpful content update',
-    '2022-10-19': 'Oct 2022 spam update',
-    '2022-09-20': 'Sep 2022 product reviews update',
-    '2022-09-12': 'Sep 2022 core update',
-    '2022-08-25': 'Aug 2022 helpful content update',
-    '2022-07-27': 'Jul 2022 product reviews update',
-    '2022-05-25': 'May 2022 core update',
-    '2022-03-23': 'Mar 2022 product reviews update',
-    '2022-02-22': 'Feb 2022 page experience update for desktop',
+    # (same content as before)
 }
 
 # Convert the keys to datetime objects
@@ -40,26 +31,29 @@ if uploaded_file is not None:
         if property_filter:
             filtered_data = filtered_data[filtered_data['property'].str.contains(property_filter)]
 
+        threshold = st.sidebar.number_input('Minimum average number of clicks:', value=0)
+
         filtered_properties = filtered_data['property'].unique()
         st.write(f"Found {len(filtered_properties)} properties")
 
         for property_name in filtered_properties:
             selected_property = filtered_data[filtered_data['property'] == property_name]
 
-            fig, ax = plt.subplots()
+            if selected_property['clicks'].mean() >= threshold:
+                fig, ax = plt.subplots()
 
-            ax.plot(selected_property['scrap_date'], selected_property['clicks'], label=property_name)
-            ax.set_xlabel('Scrap Date')
-            ax.set_ylabel('Clicks')
-            ax.set_title(f'{property_name} Clicks over Time', pad=20)
+                ax.plot(selected_property['scrap_date'], selected_property['clicks'], label=property_name)
+                ax.set_xlabel('Scrap Date')
+                ax.set_ylabel('Clicks')
+                ax.set_title(f'{property_name} Clicks over Time', pad=20)
 
-            for event_date, event_description in important_dates.items():
-                ax.axvline(pd.Timestamp(event_date), color='red', linestyle='--', alpha=0.5)
-                ax.text(pd.Timestamp(event_date), ax.get_ylim()[1], event_description, rotation=45, ha='left', va='top', fontsize=8)
+                for event_date, event_description in important_dates.items():
+                    ax.axvline(pd.Timestamp(event_date), color='red', linestyle='--', alpha=0.5)
+                    ax.text(pd.Timestamp(event_date), ax.get_ylim()[1] * 1.05, event_description, rotation=45, ha='left', va='bottom', fontsize=8)
 
-            ax.legend(loc='upper left', fontsize=8)
-            ax.xaxis.set_major_formatter(DateFormatter("%Y-%m-%d"))
-            plt.xticks(rotation=45)
-            plt.tight_layout()
-            st.pyplot(fig)
-            plt.close(fig)
+                ax.legend(loc='upper left', fontsize=8)
+                ax.xaxis.set_major_formatter(DateFormatter("%Y-%m-%d"))
+                plt.xticks(rotation=45)
+                plt.tight_layout()
+                st.pyplot(fig)
+                plt.close(fig)
