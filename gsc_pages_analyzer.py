9015c3b2faac4ad83720e9ab0478e1db1484b44c
TomekPLR <trudzki@gmail.com>
2023-04-23 21:57:56 +0200

    Update gsc_pages_analyzer.py

--- gsc_pages_analyzer.py
+++ gsc_pages_analyzer.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from datetime import datetime, timedelta
+
+# Define the default date range
+default_end_date = datetime.today() - timedelta(days=1)
+default_start_date = default_end_date - timedelta(days=14)
 
 # Define the file uploader
 uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
@@ -14,22 +19,16 @@ if uploaded_file is not None:
     pages_df["Date"] = pd.to_datetime(pages_df["Date"], format="%b %d, %Y")
 
     # Define the date range selector
-    date_range = st.sidebar.date_input("Select a date range:", [pages_df["Date"].min(), pages_df["Date"].max()])
+    start_date = st.sidebar.date_input("Select a start date", default_start_date)
+    end_date = st.sidebar.date_input("Select an end date", default_end_date)
 
     # Filter the pages data by date range
-    pages_filtered = pages_df[(pages_df["Date"] >= pd.to_datetime(date_range[0].strftime('%Y-%m-%d'))) & (pages_df["Date"] <= pd.to_datetime(date_range[1].strftime('%Y-%m-%d')))]
+    pages_filtered = pages_df[(pages_df["Date"] >= start_date) & (pages_df["Date"] <= end_date)]
 
-    # Define the country selector
-    countries = st.sidebar.multiselect("Select countries:", pages_filtered["Country"].unique())
-
-    # Filter the pages data by selected countries
-    if countries:
-        pages_filtered = pages_filtered[pages_filtered["Country"].isin(countries)]
-
-    # Create a bar chart of clicks by country and date
+    # Create a line chart of clicks by country and date
     clicks_by_country = pages_filtered.groupby(["Country", "Date"])["Url Clicks"].sum().reset_index()
     fig1, ax1 = plt.subplots()
-    sns.barplot(x="Date", y="Url Clicks", hue="Country", data=clicks_by_country, ax=ax1)
+    sns.lineplot(x="Date", y="Url Clicks", hue="Country", data=clicks_by_country, ax=ax1)
     ax1.set_title("Clicks by Country")
     st.pyplot(fig1)
 
