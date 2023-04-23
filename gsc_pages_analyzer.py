df8acdddb71da49aba817d5d73bdf6a3cad0f275
TomekPLR <trudzki@gmail.com>
2023-04-23 22:29:56 +0200

    Update gsc_pages_analyzer.py

--- gsc_pages_analyzer.py
+++ gsc_pages_analyzer.py
@@ -1,13 +1,22 @@
-# Define the default number of countries and catalogs to show
-default_num_countries = 5
-default_num_catalogs = 5
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime, timedelta
+
+# Define the default date range
+default_end_date = datetime.today() - timedelta(days=1)
+default_start_date = default_end_date - timedelta(days=14)
 
 # Define the file uploader
 uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
 
 if uploaded_file is not None:
     # Load the CSV file
-    pages_df = pd.read_csv(uploaded_file)
+    try:
+        pages_df = pd.read_csv(uploaded_file)
+    except Exception as e:
+        st.error("Error: Could not read CSV file. Please check your file format.")
 
     # Convert the "Date" column to datetime format
     pages_df["Date"] = pd.to_datetime(pages_df["Date"], format="%b %d, %Y")
@@ -15,7 +24,6 @@ if uploaded_file is not None:
     # Define the date range selector
     start_date = st.sidebar.date_input("Select a start date", default_start_date)
     end_date = st.sidebar.date_input("Select an end date", default_end_date)
-    
     start_date = datetime.strptime(str(start_date), '%Y-%m-%d')
     end_date = datetime.strptime(str(end_date), '%Y-%m-%d')
 
@@ -24,31 +32,32 @@ if uploaded_file is not None:
 
     # Create a chart of clicks by country and date
     clicks_by_country = pages_filtered.groupby(["Country", "Date"])["Url Clicks"].sum().reset_index()
+    if not clicks_by_country.empty:
+        fig1, ax1 = plt.subplots()
+        sns.lineplot(x="Date", y="Url Clicks", hue="Country", data=clicks_by_country, ax=ax1)
+        ax1.set_title("Clicks by Country")
+        st.pyplot(fig1)
+    else:
+        st.warning("No data found for selected date range.")
 
-    # Get the top countries by clicks
-    top_countries = clicks_by_country.groupby("Country")["Url Clicks"].sum().sort_values(ascending=False)
-
-    # Define the country selector
-    countries_selected = st.sidebar.multiselect("Select countries:", top_countries.index, default=top_countries.index[:default_num_countries])
-
-    # Filter the pages data by selected countries
-    pages_countries = pages_filtered[pages_filtered["Country"].isin(countries_selected)]
-
-    # Create a chart of clicks by catalog and date
-    catalogs = pages_countries["Landing Page"].apply(lambda x: x.split("/")[1]).unique()
-    catalogs_selected = st.sidebar.multiselect("Select catalogs:", catalogs, default=catalogs[:default_num_catalogs])
-
+    # Define the catalog selector
+    catalogs = pages_filtered["Landing Page"].apply(lambda x: x.split("/")[1]).unique()
+    default_num_catalogs = 5
+    top_catalogs = pages_filtered.groupby("Landing Page")["Url Clicks"].sum().sort_values(ascending=False).head(default_num_catalogs)
+    catalogs_selected = st.sidebar.multiselect("Select catalogs:", top_catalogs.index, default=top_catalogs.index)
+    
     # Define the metric selector
     metric = st.sidebar.selectbox("Select a metric:", ["Url Clicks", "Impressions", "URL CTR"])
 
     # Filter the pages data by selected catalogs
-    pages_catalogs = pages_countries[pages_countries["Landing Page"].apply(lambda x: x.split("/")[1]).isin(catalogs_selected)]
+    pages_catalogs = pages_filtered[pages_filtered["Landing Page"].apply(lambda x: x.split("/")[1]).isin(catalogs_selected)]
 
     # Pivot the pages data to create a chart of clicks by catalog and date
     pivot = pd.pivot_table(pages_catalogs, values=metric, index="Date", columns=pages_catalogs["Landing Page"].apply(lambda x: x.split("/")[1]), aggfunc=sum)
-
-    # Plot the chart
-    fig2, ax2 = plt.subplots()
-    pivot.plot(ax=ax2)
-    ax2.set_title("Clicks by Catalog")
-    st.pyplot(fig2)
+    if not pivot.empty:
+        fig2, ax2 = plt.subplots()
+        pivot.plot(ax=ax2)
+        ax2.set_title(f"{metric} by Catalog")
+        st.pyplot(fig2)
+    else:
+        st.warning("No data found for selected catalogs and date range.")
