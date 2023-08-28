e217a35c331095db1192e64578ca7d87511cd9d2
TomekPLR <trudzki@gmail.com>
2023-08-28 15:14:05 +0400

    Update indexing_trends.py
    add property selector

--- indexing_trends.py
+++ indexing_trends.py
@@ -5,30 +5,29 @@ import matplotlib.pyplot as plt
 
 def plot_trends(df, columns, filter_dates):
     for col in columns:
-        plt.figure(figsize=(12, 6))
-
+        st.write(f"### Trendline for {col}")  # Headline for the chart
+        fig, ax = plt.subplots(figsize=(12, 6))
+        
         # Convert filter_dates to Pandas Timestamp for compatible filtering
         filter_start = pd.Timestamp(filter_dates[0])
         filter_end = pd.Timestamp(filter_dates[1])
-
+        
         # Filter based on selected date range
         df_filtered = df[(df['scrap_date'] >= filter_start) & (df['scrap_date'] <= filter_end)]
-
-        plt.scatter(df_filtered['scrap_date'], df_filtered[col], label='Data Points')
-
+        
+        ax.scatter(df_filtered['scrap_date'], df_filtered[col], label='Data Points')
+        
         # Adding Trendline
         z = np.polyfit(df_filtered.index, df_filtered[col], 1)
         p = np.poly1d(z)
-        plt.plot(df_filtered['scrap_date'], p(df_filtered.index), 'r--', label='Trendline')
-
-        plt.title(f"Trendline for {col}")
-        plt.xlabel("Scrap Date")
-        plt.ylabel(f"{col} (%)")
-        plt.legend()
-        plt.grid(True)
-
-        st.pyplot()
-
+        ax.plot(df_filtered['scrap_date'], p(df_filtered.index), 'r--', label='Trendline')
+        
+        ax.set_xlabel("Scrap Date")
+        ax.set_ylabel(f"{col} (%)")
+        ax.legend()
+        ax.grid(True)
+        
+        st.pyplot(fig)  # Avoiding the warning by passing the figure object
 
 # Main App
 st.title("CSV Trendline Analysis")
@@ -43,6 +42,12 @@ if uploaded_file:
     else:
         df['scrap_date'] = pd.to_datetime(df['scrap_date'])
         
+        # Property selection
+        properties_to_show = st.sidebar.multiselect('Select properties', df['property'].unique())
+        
+        if properties_to_show:
+            df = df[df['property'].isin(properties_to_show)]
+        
         # Sidebar for date selection
         min_date = df['scrap_date'].min()
         max_date = df['scrap_date'].max()
@@ -55,8 +60,8 @@ if uploaded_file:
         # Sorting dataframe by scrap_date for plotting
         df.sort_values('scrap_date', inplace=True)
         
-        # Grouping by scrap_date
-        df_grouped = df.groupby('scrap_date')[trend_columns].mean().reset_index()
+        # Grouping by scrap_date and selected properties
+        df_grouped = df.groupby(['scrap_date', 'property'])[trend_columns].mean().reset_index()
         
         # Plotting
         plot_trends(df_grouped, trend_columns, filter_dates)
