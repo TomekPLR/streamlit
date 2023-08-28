db52c1e05589f68e90c608760e4c4791db516471
TomekPLR <trudzki@gmail.com>
2023-08-28 15:01:58 +0400

    Update indexing_trends.py
    gtp 4

--- indexing_trends.py
+++ indexing_trends.py
@@ -9,19 +9,19 @@ def plot_trends(df, columns, filter_dates):
         
         # Filter based on selected date range
         if filter_dates:
-            df_filtered = df[(df['scrape_date'] >= filter_dates[0]) & (df['scrape_date'] <= filter_dates[1])]
+            df_filtered = df[(df['scrap_date'] >= filter_dates[0]) & (df['scrap_date'] <= filter_dates[1])]
         else:
             df_filtered = df
         
-        plt.scatter(df_filtered['scrape_date'], df_filtered[col], label='Data Points')
+        plt.scatter(df_filtered['scrap_date'], df_filtered[col], label='Data Points')
         
         # Adding Trendline
         z = np.polyfit(df_filtered.index, df_filtered[col], 1)
         p = np.poly1d(z)
-        plt.plot(df_filtered['scrape_date'], p(df_filtered.index), 'r--', label='Trendline')
+        plt.plot(df_filtered['scrap_date'], p(df_filtered.index), 'r--', label='Trendline')
         
         plt.title(f"Trendline for {col}")
-        plt.xlabel("Scrape Date")
+        plt.xlabel("Scrap Date")
         plt.ylabel(f"{col} (%)")
         plt.legend()
         plt.grid(True)
@@ -36,25 +36,25 @@ uploaded_file = st.file_uploader("Upload a CSV file:", type=["csv"])
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
     
-    if 'Property' not in df.columns or 'scrape_date' not in df.columns:
-        st.error("The CSV file must contain 'Property' and 'scrape_date' columns.")
+    if 'property' not in df.columns or 'scrap_date' not in df.columns:
+        st.error("The CSV file must contain 'property' and 'scrap_date' columns.")
     else:
-        df['scrape_date'] = pd.to_datetime(df['scrape_date'])
+        df['scrap_date'] = pd.to_datetime(df['scrap_date'])
         
         # Sidebar for date selection
-        min_date = df['scrape_date'].min()
-        max_date = df['scrape_date'].max()
+        min_date = df['scrap_date'].min()
+        max_date = df['scrap_date'].max()
         
         filter_dates = st.sidebar.date_input("Filter dates:", [min_date, max_date])
         
         # Columns to consider for trendlines
         trend_columns = [col for col in df.columns if 'pct_affected' in col]
         
-        # Sorting dataframe by scrape_date for plotting
-        df.sort_values('scrape_date', inplace=True)
+        # Sorting dataframe by scrap_date for plotting
+        df.sort_values('scrap_date', inplace=True)
         
-        # Grouping by scrape_date
-        df_grouped = df.groupby('scrape_date')[trend_columns].mean().reset_index()
+        # Grouping by scrap_date
+        df_grouped = df.groupby('scrap_date')[trend_columns].mean().reset_index()
         
         # Plotting
         plot_trends(df_grouped, trend_columns, filter_dates)
