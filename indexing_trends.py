9d34924fd3a0770ffe6f9dc765f02ff9e7e74e43
TomekPLR <trudzki@gmail.com>
2023-08-28 18:15:31 +0400

    Update indexing_trends.py

--- indexing_trends.py
+++ indexing_trends.py
@@ -1,54 +1,51 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-
-# Function to plot trend
-def plot_trend(df, title):
-    plt.figure(figsize=(12, 6))
-    plt.title(title)
-    plt.plot(df['scrap_date'], df['median_value'])
-    plt.xticks(rotation=45)
-    plt.xlabel('Date')
-    plt.ylabel('Median Value')
-    st.pyplot()
-
-# Main App
-st.title("CSV Trendline Analysis")
+from datetime import datetime
 
 # Upload CSV file
-uploaded_file = st.file_uploader("Upload a CSV file:", type=["csv"])
+uploaded_file = st.sidebar.file_uploader("Upload CSV", type=["csv"])
 
-if uploaded_file:
+# Read CSV
+if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    st.write(df.dtypes)  # Debug line to write the data types
+    
+    # Ensure the necessary columns exist
+    if all(col in df.columns for col in ["property", "scrap_date"]):
+        # Convert 'scrap_date' to datetime and drop invalid rows
+        df['scrap_date'] = pd.to_datetime(df['scrap_date'], errors='coerce')
+        df.dropna(subset=['scrap_date'], inplace=True)
+        
+        min_date = df['scrap_date'].min().date()
+        max_date = df['scrap_date'].max().date()
 
-    # Check if the required columns are present
-    if 'property' not in df.columns or 'scrap_date' not in df.columns:
-        st.error('CSV file must have columns named "property" and "scrap_date".')
+        if min_date and max_date:
+            # Sidebar
+            st.sidebar.title("Filters")
+            
+            # Date range selector
+            selected_date_range = st.sidebar.date_input('Select Date Range:', [min_date, max_date])
+            
+            # Property selector
+            property_list = df['property'].unique().tolist()
+            selected_properties = st.sidebar.multiselect('Select Properties:', ["All"] + property_list, default=["All"])
+            
+            # Filter data
+            if "All" not in selected_properties:
+                df_filtered = df[df['property'].isin(selected_properties)]
+            else:
+                df_filtered = df
+            
+            df_filtered = df_filtered[(df_filtered['scrap_date'].dt.date >= selected_date_range[0]) & (df_filtered['scrap_date'].dt.date <= selected_date_range[1])]
+            
+            # Group and calculate the median
+            df_grouped = df_filtered.groupby('scrap_date').median().reset_index()
+
+            # Display data
+            st.write(df_grouped)
+        else:
+            st.write("Data does not contain valid min and max dates.")
     else:
-        # Sidebar for date range selection
-        min_date = df['scrap_date'].min()
-        max_date = df['scrap_date'].max()
-        selected_date_range = st.sidebar.date_input('Select Date Range:', [min_date, max_date])
-        
-        df['scrap_date'] = pd.to_datetime(df['scrap_date'])
-        df_filtered_date = df[(df['scrap_date'] >= pd.Timestamp(selected_date_range[0])) & 
-                               (df['scrap_date'] <= pd.Timestamp(selected_date_range[1]))]
-        
-        # Find columns that start with "pct"
-        pct_columns = [col for col in df.columns if col.startswith("pct")]
+        st.write("CSV file must have columns named 'property' and 'scrap_date'.")
 
-        # Sidebar for property selection
-        selected_pct_columns = st.sidebar.multiselect("Choose Columns:", pct_columns, default=pct_columns)
-        
-        # Filter DataFrame
-        df_filtered = df_filtered_date[selected_pct_columns + ['scrap_date']]
-        
-        # Calculate the median value and add it to the dataframe
-        df_filtered['median_value'] = df_filtered[selected_pct_columns].median(axis=1)
-
-        # Group by date and find median
-        df_grouped = df_filtered.groupby('scrap_date')['median_value'].median().reset_index()
-
-        # Plotting
-        plot_trend(df_grouped, 'Trendline for Selected Properties and Date Range')
+else:
+    st.write("Please upload a CSV file.")
