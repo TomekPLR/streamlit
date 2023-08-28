9b3b677b8a7de84592bbfdf051c728321d116d57
TomekPLR <trudzki@gmail.com>
2023-08-28 11:59:38 +0400

    Update indexing_trends.py
    version from gpt4

--- indexing_trends.py
+++ indexing_trends.py
@@ -1,40 +1,60 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-# Function to load the CSV file
-def load_data(file):
-    data = pd.read_csv(file)
-    return data
-
-# Function to display the trendline for a specific column
-def display_trendline(data, column):
-    plt.figure(figsize=(10, 6))
-    sns.lineplot(x="scrape_date", y=column, data=data)
-    plt.title(f"Trendline for {column}")
-    plt.xlabel("Scrape Date")
-    plt.ylabel("Percentage Affected")
-    st.pyplot()
-
-# Main function
-def main():
-    st.title("CSV Data Analysis")
-    st.sidebar.title("Settings")
-
-    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-    
-    if uploaded_file is not None:
-        data = load_data(uploaded_file)
+def plot_trends(df, columns, filter_dates):
+    for col in columns:
+        plt.figure(figsize=(12, 6))
         
-        # Date filtering
-        date_filter = st.sidebar.date_input("Select date range", [data['scrape_date'].min(), data['scrape_date'].max()])
-        filtered_data = data[(data['scrape_date'] >= date_filter[0]) & (data['scrape_date'] <= date_filter[1])]
+        # Filter based on selected date range
+        if filter_dates:
+            df_filtered = df[(df['scrape_date'] >= filter_dates[0]) & (df['scrape_date'] <= filter_dates[1])]
+        else:
+            df_filtered = df
+        
+        plt.scatter(df_filtered['scrape_date'], df_filtered[col], label='Data Points')
+        
+        # Adding Trendline
+        z = np.polyfit(df_filtered.index, df_filtered[col], 1)
+        p = np.poly1d(z)
+        plt.plot(df_filtered['scrape_date'], p(df_filtered.index), 'r--', label='Trendline')
+        
+        plt.title(f"Trendline for {col}")
+        plt.xlabel("Scrape Date")
+        plt.ylabel(f"{col} (%)")
+        plt.legend()
+        plt.grid(True)
+        
+        st.pyplot()
 
-        columns_to_plot = [col for col in data.columns if col.startswith("pct_affected_")]
-        selected_column = st.sidebar.selectbox("Select column to plot", columns_to_plot)
+# Main App
+st.title("CSV Trendline Analysis")
 
-        display_trendline(filtered_data, selected_column)
+uploaded_file = st.file_uploader("Upload a CSV file:", type=["csv"])
 
-if __name__ == "__main__":
-    main()
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    
+    if 'Property' not in df.columns or 'scrape_date' not in df.columns:
+        st.error("The CSV file must contain 'Property' and 'scrape_date' columns.")
+    else:
+        df['scrape_date'] = pd.to_datetime(df['scrape_date'])
+        
+        # Sidebar for date selection
+        min_date = df['scrape_date'].min()
+        max_date = df['scrape_date'].max()
+        
+        filter_dates = st.sidebar.date_input("Filter dates:", [min_date, max_date])
+        
+        # Columns to consider for trendlines
+        trend_columns = [col for col in df.columns if 'pct_affected' in col]
+        
+        # Sorting dataframe by scrape_date for plotting
+        df.sort_values('scrape_date', inplace=True)
+        
+        # Grouping by scrape_date
+        df_grouped = df.groupby('scrape_date')[trend_columns].mean().reset_index()
+        
+        # Plotting
+        plot_trends(df_grouped, trend_columns, filter_dates)
