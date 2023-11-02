f25120273d5d91e918a9541d553c4c66979f15e7
TomekPLR <trudzki@gmail.com>
2023-11-02 14:32:44 +0400

    Update correl_explorer.py

--- correl_explorer.py
+++ correl_explorer.py
@@ -4,7 +4,6 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Function to find most correlated pairs from a correlation matrix
 def get_most_correlated_pairs(corr_matrix):
     pairs_to_drop = set()
     cols = corr_matrix.columns
@@ -15,36 +14,42 @@ def get_most_correlated_pairs(corr_matrix):
     corrs = corr_matrix.unstack().drop(labels=pairs_to_drop).sort_values(ascending=False, key=lambda x: abs(x))
     return corrs
 
-# Streamlit app
 st.title("CSV Correlation Explorer")
-
-# Upload CSV file
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 if uploaded_file is not None:
-    # Read CSV
     df = pd.read_csv(uploaded_file)
 
-    # Select columns to exclude from analysis
+    default_exclude_columns = [
+        'status_details', 'response_301', 'response_302', 'response_moved',
+        'response_not_modified', 'response_blocked_robots', 'response_404',
+        'response_robots_not_available', 'response_dns_error', 'response_fetch_error',
+        'response_page_not_reached', 'response_page_timeout', 'response_redirect_error',
+        'response_other_error', 'purpose_refresh', 'type_mobile', 'type_desktop',
+        'type_image', 'type_video', 'type_ads', 'type_store', 'type_other',
+        'property.3', 'scrap_date.3', 'property.4', 'scrap_date.4', 'last_update',
+        'property.5', 'scrap_date.5', 'last_update.1'
+    ]
+
     st.header("Columns to Exclude")
-    exclude_columns = st.multiselect("Select columns to exclude from analysis:", df.columns.tolist())
+    exclude_columns = st.multiselect(
+        "Select additional columns to exclude from analysis:", 
+        df.columns.tolist(),
+        default=default_exclude_columns
+    )
 
-    # Filter out the excluded columns
     filtered_df = df.drop(columns=exclude_columns)
-
-    # Select only numeric columns
     numeric_df = filtered_df.select_dtypes(include=[np.number])
-
-    # Calculate correlation matrix
     corr_matrix = numeric_df.corr()
 
-    # Display correlation matrix
     st.header("Correlation Matrix")
-    fig, ax = plt.subplots(figsize=(10, 8))
-    sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap='coolwarm', ax=ax)
+    # Adjust figure size and annotation font size
+    fig, ax = plt.subplots(figsize=(max(10, len(numeric_df.columns)), max(8, len(numeric_df.columns) // 2)))
+    sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap='coolwarm', ax=ax, annot_kws={'size': 8})
+    plt.xticks(rotation=45)
+    plt.yticks(rotation=45)
     st.pyplot(fig)
 
-    # Display most correlated pairs
     st.header("Most Correlated Pairs")
     most_correlated_pairs = get_most_correlated_pairs(corr_matrix)
     st.write(most_correlated_pairs)
