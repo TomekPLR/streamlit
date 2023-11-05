1606f95fa0dfcf8af08fc6e55c05d107447f14aa
TomekPLR <trudzki@gmail.com>
2023-11-05 22:33:58 +0400

    Update gschealthcheck.py

--- gschealthcheck.py
+++ gschealthcheck.py
@@ -9,31 +9,35 @@ medians = {
     '301': 20,
     '% of requests with server errors': 24,
     '% of pages with discovery purpose': 10,
-    '% of requests for page resources': 25,
+    '% of requuests for page resources': 25,
+    '% of pages classified as Crawled currently not indedex': 100,
+    '% of URLs classfied as discovered currently not indexed': 200,
     '% of indexed pages': 272,
-    '% of pages classified as Crawled currently not indexed': 100,
-    '% of URLs classified as discovered currently not indexed': 200
 }
 
 custom_messages = {
-    '% of Good URLs': "Good job on having quality URLs!",
+    '% of Good URLs': "Good job on having a high percentage of quality URLs!",
     'Average response time': "Your average response time can be improved.",
-    'OK (200) + 304': "Ensure your server returns OK statuses.",
+    'OK (200) + 304': "Ensure your server returns more OK statuses.",
     '404': "Too many 404 errors, check your broken links.",
     '301': "Review your redirects to optimize load time.",
     '% of requests with server errors': "Investigate the server errors.",
     '% of pages with discovery purpose': "Enhance your content discoverability.",
-    '% of requests for page resources': "Optimize your page resources.",
+    '% of requuests for page resources': "Optimize your page resources.",
+    '% of pages classified as Crawled currently not indedex': "Ensure crawled pages are indexed.",
+    '% of URLs classfied as discovered currently not indexed': "Ensure discovered pages are indexed.",
     '% of indexed pages': "Increase your indexed pages.",
-    '% of pages classified as Crawled currently not indexed': "Ensure crawled pages are indexed.",
-    '% of URLs classified as discovered currently not indexed': "Ensure discovered pages are indexed.",
 }
 
-# Define field groups
+lower_is_better = [
+    'Average response time', '301', '404', 
+    '% of requests with server errors', '% of requuests for page resources',
+    '% of pages classified as Crawled currently not indedex',
+    '% of URLs classfied as discovered currently not indexed',
+]
+
 field_groups = {
-    'Core Web Vitals report': [
-        '% of Good URLs'
-    ],
+    'Core Web Vitals report': ['% of Good URLs'],
     'Crawl stats report': [
         'Average response time',
         'OK (200) + 304',
@@ -41,24 +45,13 @@ field_groups = {
         '301',
         '% of requests with server errors',
         '% of pages with discovery purpose',
-        '% of requests for page resources'
+        '% of requuests for page resources',
     ],
     'Indexing report': [
+        '% of pages classified as Crawled currently not indedex',
+        '% of URLs classfied as discovered currently not indexed',
         '% of indexed pages',
-        '% of pages classified as Crawled currently not indexed',
-        '% of URLs classified as discovered currently not indexed'
     ]
-}
-
-# Specify fields where lower is better
-lower_is_better = {
-    'Average response time',
-    '404',
-    '301',
-    '% of requests with server errors',
-    '% of requests for page resources',
-    '% of pages classified as Crawled currently not indexed',
-    '% of URLs classified as discovered currently not indexed'
 }
 
 # Input form
@@ -67,33 +60,36 @@ domain = st.text_input("Type your domain (without www) 🔗")
 
 user_values = {}
 for group, fields in field_groups.items():
-    st.subheader(group)
-    for field in fields:
-        if '%' in field:
-            user_values[field] = st.slider(f"{field} (%) 📊", 0, 100)
-        else:
-            user_values[field] = st.number_input(f"{field} 🧮", 0)
+    with st.beta_expander(group):
+        for field in fields:
+            if '%' in field:
+                user_values[field] = st.slider(f"{field} (%) 📊", 0, 100)
+            else:
+                user_values[field] = st.number_input(f"{field} 🧮", 0)
 
 # Compare to median and display result
 if st.button("Compare 🔄"):
-    st.subheader("Comparison Results:")
-    for group, fields in field_groups.items():
-        st.subheader(group)
-        for field in fields:
-            median_value = medians[field]
-            if field in lower_is_better:
-                if user_values[field] < median_value:
-                    st.write(f"{field}: **Better** than median ({median_value}) 😃")
-                elif user_values[field] > median_value:
-                    st.write(f"{field}: **Worse** than median ({median_value}) 😟")
-                    st.write(f"👉 {custom_messages[field]}")
+    with st.beta_expander("Results"):
+        st.subheader("Comparison Results:")
+        for group, fields in field_groups.items():
+            st.subheader(group)
+            for field in fields:
+                median_value = medians[field]
+                if field in lower_is_better:
+                    if user_values[field] < median_value:
+                        st.write(f"{field}: **Better** than median ({median_value}) 😃\n")
+                    elif user_values[field] > median_value:
+                        st.write(f"{field}: **Worse** than median ({median_value}) 😟")
+                        st.write(f"👉 {custom_messages[field]}\n")
+                    else:
+                        st.write(f"{field}: **Equal** to median ({median_value}) 😐\n")
                 else:
-                    st.write(f"{field}: **Equal** to median ({median_value}) 😐")
-            else:
-                if user_values[field] < median_value:
-                    st.write(f"{field}: **Lower** than median ({median_value}) 😟")
-                    st.write(f"👉 {custom_messages[field]}")
-                elif user_values[field] > median_value:
-                    st.write(f"{field}: **Higher** than median ({median_value}) 😃")
-                else:
-                    st.write(f"{field}: **Equal** to median ({median_value}) 😐")
+                    if user_values[field] < median_value:
+                        st.write(f"{field}: **Lower** than median ({median_value}) 😟")
+                        st.write(f"👉 {custom_messages[field]}\n")
+                    elif user_values[field] > median_value:
+                        st.write(f"{field}: **Higher** than median ({median_value}) 😃\n")
+                    else:
+                        st.write(f"{field}: **Equal** to median ({median_value}) 😐\n")
+                
+                st.markdown("---")  # Visual divider
