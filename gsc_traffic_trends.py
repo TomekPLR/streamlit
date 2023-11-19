98ccc66a001947b686c460408515e744916dffa0
TomekPLR <trudzki@gmail.com>
2023-11-20 00:01:34 +0400

    Update gsc_traffic_trends.py

--- gsc_traffic_trends.py
+++ gsc_traffic_trends.py
@@ -34,21 +34,16 @@ def main():
     file1 = st.file_uploader("Upload your CSV file for Query Analysis", type=['csv'], key='file1')
     file2 = st.file_uploader("Upload your CSV file for Landing Page Analysis", type=['csv'], key='file2')
 
-    # Variables to hold dataframes
-    df_queries = None
-    df_landing_pages = None
-
-    # Read files
-    if file1:
+    # Check if files are uploaded
+    if file1 and file2:
+        # Read and process files
         df_queries = pd.read_csv(file1)
         df_queries = rename_delta_columns(df_queries)
 
-    if file2:
         df_landing_pages = pd.read_csv(file2)
         df_landing_pages = rename_delta_columns(df_landing_pages)
 
-    # Analysis for the first file (Queries)
-    if df_queries is not None:
+        # Display insights
         st.header("General Info - Queries")
         top_3, top_5, top_10 = analyze_query_positions(df_queries)
         st.metric("Queries in Top 3", top_3)
@@ -63,8 +58,6 @@ def main():
         losers_queries = find_winners_losers(df_queries)[1]
         st.dataframe(losers_queries)
 
-    # Analysis for the second file (Landing Pages)
-    if df_landing_pages is not None:
         st.header("Winners - Landing Pages with Increased Clicks")
         winners_landing_pages = find_winners_losers(df_landing_pages)[0]
         st.dataframe(winners_landing_pages)
