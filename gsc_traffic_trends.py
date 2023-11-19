e35af7e679140de78e914686cb69440f475429ba
TomekPLR <trudzki@gmail.com>
2023-11-19 22:44:17 +0400

    Update gsc_traffic_trends.py

--- gsc_traffic_trends.py
+++ gsc_traffic_trends.py
@@ -60,16 +60,14 @@ def main():
     if file1:
         df1 = pd.read_csv(file1)
         # For the first CSV, we expect the pattern '% Δ' to follow 'Average Position', 'clicks', etc.
-        column_patterns_1 = {'% Δ': ['Change in Average Position (%)', 'Change in Clicks (%)', 'Change in Site CTR (%)', 'Change in Impressions (%)']}
-        df1 = rename_columns(df1, column_patterns_1)
+        df1 = rename_columns(df1)
         st.write("First Data Preview:", df1.head())
 
     if file2:
         df2 = pd.read_csv(file2)
         # For the second CSV, we expect the pattern '% Δ' to follow 'Url Clicks', 'Impressions', 'URL CTR'
         # Since 'Url Clicks' appears twice, we must specify the new names accordingly
-        column_patterns_2 = {'% Δ': ['Change in Url Clicks 1 (%)', 'Change in Url Clicks 2 (%)', 'Change in Impressions (%)', 'Change in URL CTR (%)']}
-        df2 = rename_columns(df2, column_patterns_2)
+        df2 = rename_columns(df2)
         st.write("Second Data Preview:", df2.head())
         
         st.subheader("Top Queries Analysis (First File)")
