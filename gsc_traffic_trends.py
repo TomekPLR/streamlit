236aea6e31ca83c93ea95ab5ce945ab9cb4c53e0
TomekPLR <trudzki@gmail.com>
2023-11-19 22:28:54 +0400

    Update gsc_traffic_trends.py

--- gsc_traffic_trends.py
+++ gsc_traffic_trends.py
@@ -1,77 +1,40 @@
 import streamlit as st
 import pandas as pd
 
-def rename_columns(df):
-    # Adjust this function to match your CSV file's column names
-    new_columns = []
+def rename_columns(df, column_patterns):
+    # Assuming the column patterns are given as a dictionary:
+    # column_patterns = {'% Δ': ['Change in Average Position (%)', 'Change in Clicks (%)', ...]}
     for i, col in enumerate(df.columns):
-        if '% Δ' in col:
-            new_col = f'Change in {df.columns[i-1]} (%)'
-            new_columns.append(new_col)
-        else:
-            new_columns.append(col)
-    df.columns = new_columns
+        if col.strip() in column_patterns:
+            new_col = column_patterns[col.strip()][0]
+            column_patterns[col.strip()].pop(0)
+            df.rename(columns={col: new_col}, inplace=True)
     return df
-
-def analyze_top_queries(df):
-    # Add error handling or adjust column names based on your CSV
-    try:
-        top_3 = df[df['Average Position'] <= 3]
-        top_5 = df[df['Average Position'] <= 5]
-        top_10 = df[df['Average Position'] <= 10]
-    except KeyError:
-        st.error("Column names do not match. Please check your CSV file.")
-        return None, None, None
-
-    return len(top_3), len(top_5), len(top_10)
-
-def analyze_winners_losers(df, top_n=100):
-    # Add error handling or adjust column names
-    try:
-        winners = df.sort_values(by='Change in Clicks (%)', ascending=False).head(top_n)
-        losers = df.sort_values(by='Change in Clicks (%)').head(top_n)
-    except KeyError:
-        st.error("Column names do not match. Please check your CSV file.")
-        return None, None
-
-    return winners, losers
 
 def main():
     st.title("SEO Analysis Tool")
 
     # Two file uploaders for different CSV files
-    file1 = st.file_uploader("Upload first CSV file", type=['csv'])
-    file2 = st.file_uploader("Upload second CSV file", type=['csv'])
+    file1 = st.file_uploader("Upload first CSV file", type=['csv'], key='file1')
+    file2 = st.file_uploader("Upload second CSV file", type=['csv'], key='file2')
 
-    if file1 and file2:
+    if file1:
         df1 = pd.read_csv(file1)
-        df2 = pd.read_csv(file2)
-
-        df1 = rename_columns(df1)
-        df2 = rename_columns(df2)
-
+        # For the first CSV, we expect the pattern '% Δ' to follow 'Average Position', 'clicks', etc.
+        column_patterns_1 = {'% Δ': ['Change in Average Position (%)', 'Change in Clicks (%)', 'Change in Site CTR (%)', 'Change in Impressions (%)']}
+        df1 = rename_columns(df1, column_patterns_1)
         st.write("First Data Preview:", df1.head())
+
+    if file2:
+        df2 = pd.read_csv(file2)
+        # For the second CSV, we expect the pattern '% Δ' to follow 'Url Clicks', 'Impressions', 'URL CTR'
+        # Since 'Url Clicks' appears twice, we must specify the new names accordingly
+        column_patterns_2 = {'% Δ': ['Change in Url Clicks 1 (%)', 'Change in Url Clicks 2 (%)', 'Change in Impressions (%)', 'Change in URL CTR (%)']}
+        df2 = rename_columns(df2, column_patterns_2)
         st.write("Second Data Preview:", df2.head())
 
-        # Analysis for the first file
-        st.subheader("Top Queries Analysis (First File)")
-        top_3, top_5, top_10 = analyze_top_queries(df1)
-        if top_3 is not None:
-            st.write(f"Number of queries in top 3: {top_3}")
-            st.write(f"Number of queries in top 5: {top_5}")
-            st.write(f"Number of queries in top 10: {top_10}")
-
-        # Winners and Losers Analysis for the first file
-        st.subheader("Winners and Losers Analysis (First File)")
-        winners, losers = analyze_winners_losers(df1)
-        if winners is not None and losers is not None:
-            st.write("Winners (Top 100):")
-            st.dataframe(winners)
-            st.write("Losers (Top 100):")
-            st.dataframe(losers)
-
-        # Additional analyses for the second file can be added here
-        # ...
+    # Further analysis would go here
+    # ...
 
 if __name__ == "__main__":
     main()
